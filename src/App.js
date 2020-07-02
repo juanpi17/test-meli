6c25d2d3d3540105fa8012fe2844d8f581cb2e79
Juan Pablo <juanpi17@gmail.com>
2020-07-02 20:35:24 -0300

    Implemented nav with pure html and bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
+import Container from 'react-bootstrap/Container';
 import MainSearchBar from './components/MainSearchBar/MainSearchBar';
 
 class App extends Component {
     render () {
         return (
-            <div>
+            <div className="main">
                 <MainSearchBar />
-                <div className="card">
-                    <div className="card-body">
-                    <h5 className="card-title">Steve Jobs</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-                    <p className="card-text">Stay Hungry, Stay Foolish</p>
+                <Container>
+                    <div className="card">
+                        <div className="card-body">
+                        <h5 className="card-title">Steve Jobs</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
+                        <p className="card-text">Stay Hungry, Stay Foolish</p>
+                        </div>
                     </div>
-                </div>
+                </Container>
             </div>
         );
     }
--- src/components/MainSearchBar/MainSearchBar.js
+++ src/components/MainSearchBar/MainSearchBar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
@@ -15,17 +18,63 @@ class MainSearchBar extends Component {
     render() {
         return (
 
-            <Navbar bg="light" expand="lg" className="justify-content-center">
-                <Navbar.Brand href="#home">
-                    <img src={logo} srcSet={`${logo2x} 2x`} alt='logoIcon' className="logoIcon" />
-                </Navbar.Brand>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="link">
-                        <img src={glass} srcSet={`${glass2x} 2x`} alt='glassIcon' className="glassIcon" />
-                    </Button>
-                </Form>
-            </Navbar>
+            // <Navbar bg="light" expand="lg">
+            //     <Container>
+            //         <Row>
+            //             <Col md={1, { offset: 1 }}>
+            //                 <Navbar.Brand href="#home">
+            //                     <img src={logo} srcSet={`${logo2x} 2x`} alt='logoIcon' className="logoIcon" />
+            //                 </Navbar.Brand>
+            //             </Col>
+            //             <Col md={9}>
+            //                 <Form inline>
+            //                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            //                     <Button variant="link">
+            //                         <img src={glass} srcSet={`${glass2x} 2x`} alt='glassIcon' className="glassIcon" />
+            //                     </Button>
+            //                 </Form>
+            //             </Col>
+            //         </Row>
+            //     </Container>
+            // </Navbar>
+
+            // <Navbar bg="light" expand="lg">
+            //     <Container>
+            //         <Navbar.Brand href="#home">
+            //             <img src={logo} srcSet={`${logo2x} 2x`} alt='logoIcon' className="logoIcon" />
+            //         </Navbar.Brand>
+            //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            //         <Navbar.Collapse id="basic-navbar-nav">
+            //             <Form inline>
+            //                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            //                 <Button variant="link">
+            //                     <img src={glass} srcSet={`${glass2x} 2x`} alt='glassIcon' className="glassIcon" />
+            //                 </Button>
+            //             </Form>
+            //         </Navbar.Collapse>
+            //     </Container>
+            // </Navbar>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <a class="navbar-brand" href="#home">
+                        <img src={logo} srcSet={`${logo2x} 2x`} alt='logoIcon' className="logoIcon" />
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-link" type="submit">
+                                <img src={glass} srcSet={`${glass2x} 2x`} alt='glassIcon' className="glassIcon" />
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
 
         );
     }
