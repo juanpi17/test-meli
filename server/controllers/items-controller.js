c382eb251ec51dd8aa3595cae30ede468aa5042f
Juan Pablo <juanpi17@gmail.com>
2020-07-06 21:20:23 -0300

    Generated endpoint to get all items in requested format

--- server/controllers/items-controller.js
+++ server/controllers/items-controller.js
@@ -4,7 +4,7 @@ const { response } = require('express');
 // set Mercado Libre's API URL
 const meliApiUrl = process.env.MELI_API_URL || 'https://api.mercadolibre.com'
 // define items limit when performing a search
-const limit = 1;
+const limit = 4;
 
 // Create controller for GET request to '/items/search'
 exports.itemsGetAll = async (req, res, next) => {
@@ -17,21 +17,48 @@ exports.itemsGetAll = async (req, res, next) => {
         const searchUrl = `${meliApiUrl}/sites/MLA/search?q=${queryString}&limit=${limit}`;
         axios
             .get(searchUrl)
-            .then(response => response.data.results)
-            .then(results => {
+            .then(response => {
+                return {
+                    
+                    author: {
+                        name: "Juan Pablo",
+                        lastname: "Lepore"
+                    },
 
+                    categories: response.data.filters[0].values[0].path_from_root.map(category => {
+                        return category.name
+                    }),
 
+                    // we've added 2 extra fields to the mentioned on the description of the test: 
+                    // picture2x (to avoid image blurriness), and location (to make it appear on the result list)
+                    items: response.data.results.map(result => {
+                        return {
+                            id: result.id,
+                            title: result.title,
+                            price: {
+                                currency: result.currency_id,
+                                amount: Math.trunc(result.price),
+                                decimals: (result.price + "").indexOf(".") != -1 ? (result.price + "").split(".")[1] : 0,
+                            },
+                            picture: result.thumbnail,
+                            picture2x: result.thumbnail.replace(/(I\.[\w\d_-]+)$/i, 'I$1'),
+                            condition: result.condition,
+                            free_shipping: result.shipping.free_shipping,
+                            location: result.address.state_name,
+                        }
+                    })
 
-                res.send(results)
+                }
             })
+            .then(result => res.send(result))
             .catch(error => {
-                res.status(error.response.data.status).send(error.response.data);
+                next(error.data)
             });
-            // .then(response => res.send(response))
-
     }
 }
 
 exports.itemsGetOne = async ({ params: { id } }, res, next) => {
   res.json({name: 'juanpi'})
+
+//   picture: result.thumbnail.replace(/(-I)(\.[\w\d_-]+)$/i, '-F$2'),
 }
