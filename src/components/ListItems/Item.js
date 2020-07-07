675c8d37f32979e4e683b90902aac0308af6f637
Juan Pablo <juanpi17@gmail.com>
2020-07-06 22:30:21 -0300

    Added call to endpoint from results list

--- src/components/ListItems/Item.js
+++ src/components/ListItems/Item.js
@@ -9,26 +9,30 @@ import imagePlaceholder from './../../assets/image-placeholder.jpg';
 const Item = (props) => {
     // get the price and apply required format
     // let price = Math.trunc(props.product.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    const item = props.item;
 
     return (
-        <Link className="item-result" to={`/items/1`}>
+        
+        <Link className="item-result" to={`/items/${item.id}`}>
+
+        {console.log(props)}
         {/* <Link to={`/items/${props.product.id}`}> */}
             <div className="row item-body">
                 <div className="col-md-auto px-0">
-                    <img src={imagePlaceholder} alt='placeholder' className="thumb-image" />
+                    <img src={ item.picture2x } alt='placeholder' className="thumb-image" />
                 </div>
                 <div className="col-md-6">
                     <div className="price-shipping mt-2">
-                        <span className="d-inline-block pr-2 price">$ 1.980</span>
+                        <span className="d-inline-block pr-2 price">{`$ ${ item.price.amount }`}</span>
                         <img src={shippingIcon} srcSet={`${shippingIcon2x} 2x`} alt='shipping-icon' className="img-fluid" />
                     </div>
                     <div className="description">
-                        <span>Apple Ipod Touch 5g 16gb Negro Igual A Nuevo Completo Unico!</span>
+                        <span>{ item.title }</span>
                     </div>
                 </div>
                 <div className="col-md offset-md-1">
                     <div className="location mt-4">
-                        <span>{ props.location }</span>
+                        <span>{ item.location }</span>
                     </div>
                 </div>
             </div>
--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -6,23 +6,39 @@ import Item from './Item';
 class ListItems extends Component {
 
     state = {
-        results: [
-            {
-                location: "Santa Fe"
-            },
-            {
-                location: "Capital Federal"
-            },
-            {
-                location: "Mendoza"
-            }
-        ]
+        results: []
+        // results: [
+        //     {
+        //         location: "Santa Fe"
+        //     },
+        //     {
+        //         location: "Capital Federal"
+        //     },
+        //     {
+        //         location: "Mendoza"
+        //     }
+        // ]
     };
 
     componentDidMount() {
+    // componentDidUpdate() {
+        this.performSearch(this.props.location.search)
+    //     // document.title = `You clicked ${this.state.count} times`;
+    //     console.log(this.state.searchString);
+    //     // console.log(this.state.searchString.replace(/[&\/\\#,+()$~%.'":*?<>={}]/g,'_'));
+    }
+
+    performSearch = async (query) => {
         const searchValues = queryString.parse(this.props.location.search);
         console.log(searchValues); // "search"
 
+        // create a request to the local endpoint
+        const resultsList = await fetch(this.props.location.search)
+            .then(res => res.json())
+            .then(results => this.setState({
+                results: results.items
+            }));
+            // .then(results => console.log(results.items));
         // // fetchItems = async () => {
         // const user = await fetch('/users/1')
         //     .then(res => res.json()) // Process the incoming data
@@ -32,15 +48,17 @@ class ListItems extends Component {
         // this.setState({ searchString: user });
     
         // // }
-    }
+    };
 
     render() {
         return (
 
             <div className="container results-list">
-                {this.state.results.map( result => 
-                    <Item location={ result.location } />
-                )}
+                {this.state.results && 
+                this.state.results.length > 0 && 
+                this.state.results.map( item => (
+                    <Item key={ item.id } item={ item } />
+                ))}
             </div>
 
         )
