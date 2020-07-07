6b6967de5d438abf9ba47c32d82ee2a81b19b2f5
Juan Pablo <juanpi17@gmail.com>
2020-07-06 21:47:20 -0300

    Generated endpoint to get a specific item in requested format

--- server/controllers/items-controller.js
+++ server/controllers/items-controller.js
@@ -7,14 +7,15 @@ const meliApiUrl = process.env.MELI_API_URL || 'https://api.mercadolibre.com'
 const limit = 4;
 
 // Create controller for GET request to '/items/search'
-exports.itemsGetAll = async (req, res, next) => {
+exports.itemsGetAll = async (req, res) => {
 
     // if there is a search string on the "search" param
     const queryString = req.query.search;
     if (queryString) {
 
-        // create entire request
+        // create uri to request
         const searchUrl = `${meliApiUrl}/sites/MLA/search?q=${queryString}&limit=${limit}`;
+
         axios
             .get(searchUrl)
             .then(response => {
@@ -29,7 +30,7 @@ exports.itemsGetAll = async (req, res, next) => {
                         return category.name
                     }),
 
-                    // we've added 2 extra fields to the mentioned on the description of the test: 
+                    // I've added 2 extra fields to the mentioned on the description of the test: 
                     // picture2x (to avoid image blurriness), and location (to make it appear on the result list)
                     items: response.data.results.map(result => {
                         return {
@@ -51,14 +52,67 @@ exports.itemsGetAll = async (req, res, next) => {
                 }
             })
             .then(result => res.send(result))
-            .catch(error => {
-                next(error.data)
+            .catch(errors => {
+                console.error(errors);
             });
     }
 }
 
-exports.itemsGetOne = async ({ params: { id } }, res, next) => {
-  res.json({name: 'juanpi'})
+exports.itemsGetOne = async (req, res) => {
 
-//   picture: result.thumbnail.replace(/(-I)(\.[\w\d_-]+)$/i, '-F$2'),
+    // obtain id of the item
+    const id = req.params.id;
+
+    if (id) {
+
+        // we need data from 2 endpoints, so let's define them here 
+        const itemDataUrl = `${meliApiUrl}/items/${id}`;
+        const itemDescriptionUrl = `${meliApiUrl}/items/${id}/description`;
+        
+        axios
+            .all([axios.get(itemDataUrl), axios.get(itemDescriptionUrl)])
+            .then(
+                axios.spread((...responses) => {
+
+                    // start processing when both responses are ready
+                    if (responses.length == 2) {
+
+                        // obtain data from each request
+                        const itemData = responses[0].data;
+                        const itemDescription = responses[1].data;
+
+                        return {
+
+                            author: {
+                                name: "Juan Pablo",
+                                lastname: "Lepore"
+                            },
+
+                            // as I did before, I've added an extra 'pictureFull' field to render an image with a bigger resolution 
+                            item: {
+                                id: itemData.id,
+                                title: itemData.title,
+                                price: {
+                                    currency: itemData.currency_id,
+                                    amount: Math.trunc(itemData.price),
+                                    decimals: (itemData.price + "").indexOf(".") != -1 ? (itemData.price + "").split(".")[1] : 0,
+                                },
+                                picture: itemData.thumbnail,
+                                pictureFull: itemData.thumbnail.replace(/(-I)(\.[\w\d_-]+)$/i, '-F$2'),
+                                condition: itemData.condition,
+                                free_shipping: itemData.shipping.free_shipping,
+                                sold_quantity: itemData.sold_quantity,
+                                description: itemDescription.plain_text
+                            }
+                            
+                        }
+                    }
+
+                })
+            )
+            .then(result => res.send(result))
+            .catch(errors => {
+                console.error(errors);
+            });
+    }
 }
