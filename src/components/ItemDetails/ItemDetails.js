43525ec7285b0ff82d39a5c428dbcefb458f28ee
Juan Pablo <juanpi17@gmail.com>
2020-07-07 21:57:10 -0300

    Implemented detail view with data from endpoint

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,48 +1,130 @@
 import React, { Component } from 'react';
 import Breadcrumbs from '../BreadCrumbs/BreadCrumbs';
+import { withRouter } from "react-router-dom";
 import imagePlaceholder from './../../assets/image-placeholder.jpg';
 
 class ItemDetail extends Component {
+
+    state = {
+        details: [],
+        loading: true,
+
+        // results: [
+        //     {
+        //         location: "Santa Fe"
+        //     },
+        //     {
+        //         location: "Capital Federal"
+        //     },
+        //     {
+        //         location: "Mendoza"
+        //     }
+        // ]
+    };
+
+    // componentDidUpdate(prevProps) {
+    //     // only perform search if the search parameter has changed
+    //     if (prevProps.location.search !== this.props.location.search) {
+    //         this.performSearch();
+    //     }
+    // }
+
+    componentDidMount() {
+        // obtain id of the item
+        //const id = this.props.match.params.id;
+        this.obtainItem();
+
+        console.log(this.props.match);
+    //     // document.title = `You clicked ${this.state.count} times`;
+    //     console.log(this.state.searchString);
+    //     // console.log(this.state.searchString.replace(/[&\/\\#,+()$~%.'":*?<>={}]/g,'_'));
+    }
+
+    obtainItem = async () => {
+        // const searchValues = queryString.parse(this.props.location);
+        // console.log(this.props); // "search"
+
+        // create a request to the local endpoint
+        const resultsList = await fetch(this.props.match.url)
+            .then(res => res.json())
+            .then(results => this.setState({
+                details: results.item,
+                loading: false
+            }));
+        // // fetchItems = async () => {
+        // const user = await fetch('/users/1')
+        //     .then(res => res.json()) // Process the incoming data
+
+        // // Update usersList state
+        // // setUsersList(users)
+        // this.setState({ searchString: user });
+    
+        // // }
+    };
+
     render() {
+
         return (
-            <React.Fragment>
-                <Breadcrumbs />
-                <div className="item-details">
-                    <div className="container">
-                        <div className="row details-page">
-                            <div className="col-md-9 pl-0 details-left">
-                                <div className="d-flex">
-                                    <img src={imagePlaceholder} alt='placeholder' className="main-image m-auto" />
-                                </div>
-                                <div className="description">
-                                    <div className="description-title">
-                                        <span>Descripción del producto</span>
+
+            <section className="results-section">
+            {
+                (this.state.loading)
+                ? <p>Loading</p>
+                : <React.Fragment>
+                    <Breadcrumbs />
+                    <div className="item-details">
+                        <div className="container">
+                            <div className="row details-page">
+                                <div className="col-md-9 pl-0 details-left">
+                                    <div className="image-container">
+                                        <img src={this.state.details.picture} srcSet={`${this.state.details.pictureFull} 2x`} alt='placeholder' className="main-image m-auto" />
                                     </div>
-                                    <div className="description-body">
-                                        <span>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id neque euismod, efficitur lacus vitae, laoreet odio. Proin vel tortor a massa vulputate tincidunt. Fusce at lectus libero. Nam lectus est, blandit in facilisis sit amet, vulputate vel purus. Proin eget tincidunt magna, nec sollicitudin diam. Maecenas vitae ante id nisl cursus semper. Vivamus tincidunt efficitur eros at placerat. Aliquam commodo leo vel odio efficitur consequat. Aliquam condimentum nisi tortor, nec facilisis nisi suscipit vitae. Donec metus urna, suscipit eget consectetur at, volutpat quis urna. Cras vel nisl imperdiet, scelerisque nunc sit amet, tincidunt sapien. Aliquam eleifend, lorem et ullamcorper dignissim, urna sapien maximus massa, ut scelerisque lectus sapien ac nisl. Etiam metus nibh, maximus vitae libero vitae, tempus dictum felis. Nunc nec massa vitae nisl vestibulum molestie a euismod nisl. Donec lacinia sem eget tempor convallis.
+                                    <div className="description">
+                                        <div className="description-title">
+                                            <span>Descripción del producto</span>
+                                        </div>
+                                        <div className="description-body">
+                                            <span>
+                                                {this.state.details.description}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md details-right pr-0">
+                                    <div className="item-condition-quantity">
+                                        <span>{this.state.details.condition}
+                                        {
+                                            (this.state.details.sold_quantity > 0)
+                                                ? ` - ${this.state.details.sold_quantity} 
+                                                    ${(this.state.details.sold_quantity !== 1)
+                                                    ? 'vendidos'
+                                                    : 'vendido' 
+                                                    } `
+                                                : ''
+                                        }
                                         </span>
                                     </div>
+                                    <div className="item-title">
+                                        <span>{this.state.details.title}</span>
+                                    </div>
+                                    <div className="item-price">
+                                        <span className="integer-part">
+                                            $ {Math.trunc(this.state.details.price.amount).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                                            <span className="cents">
+                                                {("0" + this.state.details.price.decimals).slice(-2)}
+                                            </span>
+                                        </span>
+                                    </div>
+                                    <button className="btn btn-info w-100">Comprar</button>
                                 </div>
-                            </div>
-                            <div className="col-md details-right pr-0">
-                                <div className="item-condition-quantity">
-                                    <span>Nuevo - 234 vendidos</span>
-                                </div>
-                                <div className="item-title">
-                                    <span>Deco Reverse Sombrero Oxford</span>
-                                </div>
-                                <div className="item-price">
-                                    <span className="integer-part">$ 1.980<span className="cents">00</span></span>
-                                </div>
-                                <button className="btn btn-info w-100">Comprar</button>
                             </div>
                         </div>
                     </div>
-                </div>
-            </React.Fragment>
+                </React.Fragment>
+            }
+            </section>
         );
     }
 }
 
-export default ItemDetail;
+export default withRouter(ItemDetail);
--- src/components/ListItems/Item.js
+++ src/components/ListItems/Item.js
@@ -2,37 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import shippingIcon from './../../assets/ic_shipping.png';
 import shippingIcon2x from './../../assets/ic_shipping@2x.png.png';
-import imagePlaceholder from './../../assets/image-placeholder.jpg';
-
-// class Item extends Component {
 
 const Item = (props) => {
-    // get the price and apply required format
-    // let price = Math.trunc(props.product.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     const item = props.item;
+
+    // get the price and apply required format
+    let amount = Math.trunc(item.price.amount).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    // check if the item has free shipping
+    var free_shipping;
+    if (item.free_shipping) {
+        free_shipping = <img src={shippingIcon} srcSet={`${shippingIcon2x} 2x`} alt='shipping-icon' className="img-fluid" />;
+    } else {
+        free_shipping = "";
+    }
 
     return (
         
         <Link className="item-result" to={`/items/${item.id}`}>
-
-        {/* {console.log(props)} */}
-        {/* <Link to={`/items/${props.product.id}`}> */}
             <div className="row item-body">
                 <div className="col-md-auto px-0">
-                    <img src={ item.picture2x } alt='placeholder' className="thumb-image" />
+                    <img src={item.picture} srcSet={`${item.picture2x} 2x`} alt='thumb-image' className="thumb-image" />
                 </div>
                 <div className="col-md-6">
                     <div className="price-shipping mt-2">
-                        <span className="d-inline-block pr-2 price">{`$ ${ item.price.amount }`}</span>
-                        <img src={shippingIcon} srcSet={`${shippingIcon2x} 2x`} alt='shipping-icon' className="img-fluid" />
+                        <span className="d-inline-block pr-2 price">{`$ ${ amount }`}</span>
+                        {free_shipping} 
+                        
                     </div>
                     <div className="description">
-                        <span>{ item.title }</span>
+                        <span>{item.title}</span>
                     </div>
                 </div>
                 <div className="col-md offset-md-1">
                     <div className="location mt-4">
-                        <span>{ item.location }</span>
+                        <span>{item.location}</span>
                     </div>
                 </div>
             </div>
